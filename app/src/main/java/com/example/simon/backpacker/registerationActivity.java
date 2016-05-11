89f9f3f7674dd191d889b5d8cae495f83c4ae538
simon <goodism5082@naver.com>
2016-05-12 01:37:57 +0900

    register 5/12-1

--- app/src/main/java/com/example/simon/backpacker/registerationActivity.java
+++ app/src/main/java/com/example/simon/backpacker/registerationActivity.java
@@ -3,6 +3,12 @@ package com.example.simon.backpacker;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class registerationActivity extends AppCompatActivity {
 
@@ -10,9 +16,46 @@ public class registerationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registeration);
-/*
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
-        */
+        Button register = (Button)findViewById(R.id.register);
+        register.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailView = (EditText)findViewById(R.id.email);
+                EditText pwView = (EditText)findViewById(R.id.pw);
+                EditText pwCheckView = (EditText)findViewById(R.id.pw_check);
+
+                String email = emailView.getText().toString();
+                String pw = pwView.getText().toString();
+                String pwCheck = pwCheckView.getText().toString();
+
+                //validate email
+                if(!isValidEmail(email)) {
+                    //alarm at screen
+                }
+
+                //check email duplication
+                if(isExistEmail(email)){
+                    //alarm at screen
+                }
+
+                //validate pw
+                if(pw.compareTo(pwCheck) != 0){
+                    //alarm at screen
+                }
+
+                //send data to DB
+            }
+        });
     }
+
+    private boolean isValidEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
+
+    private boolean isExistEmail(String email){
+        //with DB connection
+        return true;
+    }
+
 }
