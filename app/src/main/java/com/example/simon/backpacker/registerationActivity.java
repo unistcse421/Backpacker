b7045a177147943a6e5e477077122b1ee6c4e3eb
simon <goodism5082@naver.com>
2016-05-12 03:11:14 +0900

    register 5/12-2 (popup?)

--- app/src/main/java/com/example/simon/backpacker/registerationActivity.java
+++ app/src/main/java/com/example/simon/backpacker/registerationActivity.java
@@ -1,16 +1,21 @@
 package com.example.simon.backpacker;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import java.util.regex.Pattern;
 
 public class registerationActivity extends AppCompatActivity {
+    private PopupWindow popup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,8 @@ public class registerationActivity extends AppCompatActivity {
         register.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
+                boolean isValidateForm = true;
+
                 EditText emailView = (EditText)findViewById(R.id.email);
                 EditText pwView = (EditText)findViewById(R.id.pw);
                 EditText pwCheckView = (EditText)findViewById(R.id.pw_check);
@@ -30,20 +37,48 @@ public class registerationActivity extends AppCompatActivity {
 
                 //validate email
                 if(!isValidEmail(email)) {
-                    //alarm at screen
-                }
-
-                //check email duplication
-                if(isExistEmail(email)){
-                    //alarm at screen
+                    TextView emailValidation = (TextView) findViewById(R.id.email_validation);
+                    emailValidation.setText("Email is Invalidate");
+                    isValidateForm = false;
+                }else if(isExistEmail(email)){                              //check email duplication
+                    TextView emailValidation = (TextView) findViewById(R.id.email_validation);
+                    emailValidation.setText("Email already Exist");
+                    isValidateForm = false;
+                }else{
+                    TextView emailValidation = (TextView) findViewById(R.id.email_validation);
+                    emailValidation.setText("");
                 }
 
                 //validate pw
-                if(pw.compareTo(pwCheck) != 0){
-                    //alarm at screen
+                if(pw.length()<8 || pw.length()>20){
+                    TextView pwValidation = (TextView) findViewById(R.id.pw_validation);
+                    pwValidation.setText("PW should be 8~20 characters");
+                    isValidateForm = false;
+                }else{
+                    TextView pwValidation = (TextView) findViewById(R.id.pw_validation);
+                    pwValidation.setText("");
                 }
 
-                //send data to DB
+                //validate pwcheck
+                if(pw.compareTo(pwCheck) != 0 || pw.length() == 0){
+                    TextView pwCheckValidation = (TextView) findViewById(R.id.pwcheck_validation);
+                    pwCheckValidation.setText("PW check is not correct");
+                    isValidateForm = false;
+                }else{
+                    TextView pwCheckValidation = (TextView) findViewById(R.id.pwcheck_validation);
+                    pwCheckValidation.setText("");
+                }
+
+                if(isValidateForm){
+                    //send data to DB
+
+                    View popupView = getLayoutInflater().inflate(R.layout.activity_registeration,null);
+                    popup = new PopupWindow(popupView);
+
+                    popup.showAtLocation(popupView, Gravity.CENTER,0,0);
+
+                    moveHome();
+                }
             }
         });
     }
@@ -58,4 +93,9 @@ public class registerationActivity extends AppCompatActivity {
         return true;
     }
 
+    private void moveHome(){
+        popup.dismiss();
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
 }
