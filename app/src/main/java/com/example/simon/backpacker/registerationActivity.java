ccd66f1296042096e84eb20436b906ba7f049fa5
simon <goodism5082@naver.com>
2016-05-12 00:50:24 +0900

    register layout

--- app/src/main/java/com/example/simon/backpacker/registerationActivity.java
+++ app/src/main/java/com/example/simon/backpacker/registerationActivity.java
@@ -1,15 +1,18 @@
 package com.example.simon.backpacker;
 
-import android.support.v7.app.ActionBar;
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class registerationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_registeration);
+/*
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+        */
     }
 }
